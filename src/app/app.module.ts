9f07dfd1037497f4bd7c1d6a4611e4fa1e4004ab
Mazahir Hashimli <mazahir89@gmail.com>
2020-10-14 16:32:13 +0200

    added routing for components paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'home', component:HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -11,6 +26,7 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
